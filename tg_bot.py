de6300d0679f90d0e21fe27ed8083d72b4f747b0
YuriSib <sib_94@vk.com>
2023-08-01 01:53:42 +0300

    Перенес удаление 2-й таблицы в начало цикла. ○ Далее в конец цикла вставлю задержку времени, которое нужно будет выждать, прежде чем повторить действие. ○ Также нужно будет исключить возможность повторого сообщения о снижении цены на одит и тот-же товар.
    (cherry picked from commit 48e47365d825c7ce6a01aade0fa8e1a4d9d7d6aa)

--- tg_bot.py
+++ tg_bot.py
@@ -1,21 +1,33 @@
 import telebot
+import os
 
-from main import second_save
+from main import save_in_excel, pars_and_save
 from collecting import hoarder
+from table_compared import compared
 
 bot = telebot.TeleBot('6419841809:AAFEiToc-LKefUbh7nkzEiusYGnHgA0NAK8')
 
+
 @bot.message_handler(commands=['start'])
-def main(message):
+def test(message):
     chat_id = message.chat.id
     bot.reply_to(message, f"Your chat ID is: {chat_id}")
 
+
 bot.send_message(674796107, "Произошло какое-то событие!")
 
-bot.polling(none_stop=True)
 
 original_table = 'original.xlsx'
 compared_table = 'compared.xlsx'
 
-hoarder()
-second_save()
+
+while True:
+    pars_and_save(compared_table)
+    list_dumping = compared(original_table, compared_table, 1)
+    for product in list_dumping:
+        bot.send_message(674796107, f'Товар: {product[0]}, id: {product[1]} \n упал в цене на {product[2]} %. \n'
+                                    f'Было: {product[3]} руб., стало: {product[4]} руб.')
+    os.remove(compared_table)
+
+
+bot.polling(none_stop=True)
