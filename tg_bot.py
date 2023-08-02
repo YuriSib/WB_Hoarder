0891fab8bc9b11dec7119fc3678e77bbef22219d
YuriSib <sib_94@vk.com>
2023-08-02 17:38:48 +0300

    Функционал цикла вынесен в функцию collecting(); Добавлено ожидание, после каждой итерации Добавлено условие, если таблица 1 существует, приступить у созданию таблицы 2 и их сравнению. ○ Также нужно будет исключить возможность повторого сообщения о снижении цены на одит и тот-же товар. ○ Далее создам выведение сообщения об ошибке в чат ○ Далее создам декоратор, замеряющий время выполнения функции

--- tg_bot.py
+++ tg_bot.py
@@ -1,21 +1,43 @@
 import telebot
+import os
+import time
 
-from main import second_save
+from main import save_in_excel, pars_and_save
 from collecting import hoarder
+from table_compared import compared
 
 bot = telebot.TeleBot('6419841809:AAFEiToc-LKefUbh7nkzEiusYGnHgA0NAK8')
 
+
 @bot.message_handler(commands=['start'])
-def main(message):
+def test(message):
     chat_id = message.chat.id
     bot.reply_to(message, f"Your chat ID is: {chat_id}")
 
+
 bot.send_message(674796107, "Произошло какое-то событие!")
 
-bot.polling(none_stop=True)
 
 original_table = 'original.xlsx'
 compared_table = 'compared.xlsx'
 
-hoarder()
-second_save()
+
+def collecting(original, compared_, percent, sleep):
+    if os.path.exists(original):
+        while True:
+            pars_and_save(compared_)
+            list_dumping = compared(original_table, compared_, percent)
+            for product in list_dumping:
+                bot.send_message(674796107, f'Товар: {product[0]}, id: {product[1]} \n упал в цене на {product[2]} %. \n'
+                                            f'Было: {product[3]} руб., стало: {product[4]} руб.')
+            os.remove(compared_)
+            time.sleep(sleep)
+    else:
+        pars_and_save(original)
+        time.sleep(sleep)
+
+
+collecting(original_table, compared_table, 5, 300)
+
+
+bot.polling(none_stop=True)
